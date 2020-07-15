577d5688781ae6ec5f332211bbeb9e24db3def11
Sohaib90 <sohaib.arshid101@gmail.com>
2020-07-16 00:29:49 +0200

    setup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/.streamlit/
+
+echo “\
+[general]\n\
+email = \”sohaib.arshid101@gmail.com\”\n\
+“ > ~/.streamlit/credentials.toml
+echo “\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+“ > ~/.streamlit/config.toml
